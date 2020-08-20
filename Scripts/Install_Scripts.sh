a0e9a3db392a5b918d63bc991064770e3eaa67b8
johnodon <johnodon@hotmail.com>
2020-08-20 13:07:43 -0400

    Update Install_Scripts.sh

--- Scripts/Install_Scripts.sh
+++ Scripts/Install_Scripts.sh
@@ -14,7 +14,7 @@ chmod a+x ./Scripts/Refresh.sh
 /bin/cp ./GenerateGamesList.py /home/pi/RetroPie/roms/moonlight/GenerateGamesList.py
 
 sudo chmod 777 /home/pi/RetroPie/roms/moonlight
-sudo chown pi:pi /home/pi/RetroPie/roms/moonlight
+sudo chown pi:pi /home/pi/RetroPie/roms/moonlight/*.*
 
 echo -e "Refresh script has been added to RetroPie\n"
 echo -e "After loading RetroPie, use the \"Refresh\" rom listed in the Moonlight system.\n"
