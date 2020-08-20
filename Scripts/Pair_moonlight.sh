f447a01e937e30185d01cc59d389b2a99eb6fa2e
johnodon <johnodon@hotmail.com>
2020-08-20 12:17:51 -0400

    Update Pair_moonlight.sh

--- Scripts/Pair_moonlight.sh
+++ Scripts/Pair_moonlight.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\nPairing Moonlight with another PC...\n"
+echo -e "\nPairing Moonlight with PC...\n"
 echo -e "Once you have input your PC's IP Address below, you will be given a PIN"
 echo -e "Input this on the PC to pair with Moonlight. \n"
 read -p "Input PC's IP Address here :`echo $'\n> '`" ip
