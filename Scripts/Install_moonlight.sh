ba77a3571c1d02386d3cbf0f8ff3eba2d3ab5f70
johnodon <johnodon@hotmail.com>
2020-08-20 12:16:41 -0400

    Update Install_moonlight.sh

--- Scripts/Install_moonlight.sh
+++ Scripts/Install_moonlight.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
 
-echo -e "\nAdding Moonlight to Sources List..."
-
-if grep -q "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" /etc/apt/sources.list; then
-    echo -e "NOTE: Moonlight Source Exists - Skipping"
-else
-    echo -e "Adding Moonlight to Sources List"
-    echo "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" >> /etc/apt/sources.list
-fi
-
-echo -e "\nFetching and installing the GPG key....\n"
-
-if [ -f /home/pi/itimmer.gpg ]
-then	
-    echo -e "NOTE: GPG Key Exists - Skipping"
-else		
-    wget http://archive.itimmer.nl/itimmer.gpg
-    chown pi:pi /home/pi/retromoonlight/itimmer.gpg
-    apt-key add itimmer.gpg		
-fi
-
 echo -e "\nUpdating System..."
 apt-get update -y
 
 echo -e "\nInstalling Moonlight..."
-cd ..
-
-sudo apt-get update -y
-
-sudo apt-get install git libopus0 libexpat1 libasound2 libudev1 libavahi-client3 libcurl4 libevdev2 libenet7 libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev cmake gcc g++ fakeroot debhelper -y
-sudo apt-get install libraspberrypi-dev -y
-
+apt-get install git libopus0 libexpat1 libasound2 libudev1 libavahi-client3 libcurl4 libevdev2 libenet7 libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev cmake gcc g++ fakeroot debhelper -y
 git clone https://github.com/irtimmer/moonlight-embedded.git
 cd moonlight-embedded
 git submodule update --init
@@ -40,7 +14,5 @@ cmake ../
 make
 sudo make install
 sudo ldconfig
-
 cd retromoonlight
-
 echo -e "\nMoonlight Installed!"
