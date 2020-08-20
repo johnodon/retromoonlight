db15b28ca6ef8b6920dfa1f0994f3b0c577d813e
johnodon <johnodon@hotmail.com>
2020-08-20 12:40:40 -0400

    Update Pair_moonlight.sh

--- Scripts/Pair_moonlight.sh
+++ Scripts/Pair_moonlight.sh
@@ -4,5 +4,7 @@ echo -e "\nPairing Moonlight with PC...\n"
 echo -e "Once you have input your PC's IP Address below, you will be given a PIN"
 echo -e "Input this on the PC to pair with Moonlight. \n"
 read -p "Input PC's IP Address here :`echo $'\n> '`" ip
-sudo -u pi moonlight pair $ip		
+sudo -u pi moonlight pair $ip
+ipaddess="$ip"
+export ipaddress
 echo -e "Pair Process Complete."	
