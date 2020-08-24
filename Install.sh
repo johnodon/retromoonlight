ad7413270f81ff9c0e14c37934812d9038f4d071
johnodon <johnodon@hotmail.com>
2020-08-24 15:14:20 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,9 +9,8 @@ echo -e " * 2: Install Moonlight"
 echo -e " * 3: Pair Moonlight to PC"
 echo -e " * 4: Install Moonlight Menu in RetroPie"
 echo -e " * 5: Install Moonlight Refresh Script in RetroPie"
-echo -e " * 6: Install Moonlight themes"
-echo -e " * 7: Remove Launch Scripts"
-echo -e " * 8: Exit"
+echo -e " * 6: Remove Launch Scripts"
+echo -e " * 7: Exit"
 
 read NUM
 case $NUM in 
@@ -20,7 +19,6 @@ case $NUM in
         sudo bash ./Scripts/Pair_moonlight.sh
         sudo bash ./Scripts/Install_moonlight_menu.sh
         sudo bash ./Scripts/Install_Scripts.sh
-        sudo bash ./Scripts/Install_themes.sh	
         ;;
     2)
         sudo bash ./Scripts/Install_moonlight.sh
@@ -38,16 +36,12 @@ case $NUM in
         sudo bash ./Scripts/Install_Scripts.sh
         sudo bash ./Install.sh
         ;;
-    6)  
-        sudo bash ./Scripts/Install_themes.sh
-        sudo bash ./Install.sh
-        ;;
-    7)
+    6)
         echo -e "\nRemoving all Moonlight launch scripts..."
         rm -rf /home/pi/RetroPie/roms/moonlight	
         sudo bash ./Install.sh
         ;;
-    8)
+    7)
         exit 1
         ;;		
     *) echo "INVALID NUMBER!" ;;
