108ad65361c6094478aef32446cabeb9c7c53d0b
johnodon <johnodon@hotmail.com>
2020-08-24 15:21:45 -0400

    Update Install_moonlight_menu.sh

--- Scripts/Install_moonlight_menu.sh
+++ Scripts/Install_moonlight_menu.sh
@@ -16,4 +16,5 @@ cp /etc/emulationstation/es_systems.cfg $DIRECTORY
 echo -e "Adding Moonlight to Systems"
 sudo sed -i -e 's|</systemList>|  <system>\n    <name>moonlight</name>\n    <fullname>Moonlight</fullname>\n    <path>~/RetroPie/roms/moonlight</path>\n    <extension>.sh .SH</extension>\n    <command>bash %ROM%</command>\n    <platform>pc</platform>\n    <theme>moonlight</theme>\n  </system>\n</systemList>|g' $DIRECTORY
 sudo chown pi:pi /opt/retropie/configs/all/emulationstation/es_systems.cfg
+cp -R /home/pi/retromoonlight/themes /etc/emulationstation/
 echo -e "\nMoonlight menu added to RetroPie..."
